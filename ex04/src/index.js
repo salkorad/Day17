5f7d3dd1143737cc9b51ad4f800ffd773849492c
salkorad <salko.radovovic@gmail.com>
2021-03-29 09:39:24 +0200

    Correction

--- ex04/src/index.js
+++ ex04/src/index.js
@@ -1,14 +1,16 @@
 const BAKING_TEMPERATURES = {
-    cake: { low: 180, high: 250 },
-    bread: { low: 150, high: 220},
-    pie: { low: 200, high: 230 }
+  cake: { low: 180, high: 250 },
+  bread: { low: 150, high: 220 },
+  pie: { low: 200, high: 230 },
 };
 
 function main(baking) {
-    // Only change code below this line
-    const { bread: { low: lowBread, high: highBread }} = BAKING_TEMPERATURES;
-    return { lowBread, highBread };
-    // Only change code above this line
+  // Only change code below this line
+  const {
+    bread: { low: lowBread, high: highBread },
+  } = baking;
+  return { lowBread, highBread };
+  // Only change code above this line
 }
 
 console.log(main(BAKING_TEMPERATURES));
