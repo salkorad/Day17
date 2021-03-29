e4d11494798d704f411abe21e2c5f3f5a76557db
salkorad <salko.radovovic@gmail.com>
2021-03-29 09:40:49 +0200

    Correction

--- ex06/src/index.js
+++ ex06/src/index.js
@@ -1,13 +1,12 @@
 const array1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
 function removeFirstTwo(list) {
-    "use strict";
-    // Only change code below this line
-    const [a,b, ...arr] = array1;
-    const array2 = [...arr]; // Change this line
+  "use strict";
+  // Only change code below this line
+  const [a, b, ...array2] = list; // Change this line
 
-    // Only change code above this line
-    return array2;
+  // Only change code above this line
+  return array2;
 }
 
 console.log(removeFirstTwo(array1));
