a3b3230581db3498cbb70a26bbf6a294e5020717
salkorad <salko.radovovic@gmail.com>
2021-03-29 09:34:43 +0200

    Update fajl

--- ex02/src/index.js
+++ ex02/src/index.js
@@ -1,14 +1,14 @@
 const MY_CAR = {
-    wheels: 4,
-    cylinders: 4,
-    size: 2.2
+  wheels: 4,
+  cylinders: 4,
+  size: 2.2,
 };
 
 function main(myCar) {
-    // Only change code below this line
-    const { cylinders, size } = MY_CAR;
-    return {cylinders, size};
-    // Only change code above this line
+  // Only change code below this line
+  const { cylinders, size } = MY_CAR;
+  return { cylinders, size };
+  // Only change code above this line
 }
 
 console.log(main(MY_CAR));
