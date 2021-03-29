5715927aa07b88d32e17fb2100bc287301b8d93c
salkorad <salko.radovovic@gmail.com>
2021-03-29 09:40:10 +0200

    Correction

--- ex05/src/index.js
+++ ex05/src/index.js
@@ -1,13 +1,11 @@
-let foo = 12, boo = 45;
+let foo = 12,
+  boo = 45;
 
 function main(foo, boo) {
-    // Only change code below this line
-    const [a, b] = [foo, boo];
-    let foo = b;
-    let boo = a;
-    
-    // Only change code above this line
-    return {foo, boo};
+  // Only change code below this line
+  [foo, boo] = [boo, foo];
+  // Only change code above this line
+  return { foo, boo };
 }
 
 console.log(main(foo, boo));
